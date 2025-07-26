aad111b84dbc08a224377304adff51f8e31ad05f
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2025-07-26 10:25:06 +0100

    Refactor HTML structure and remove redundant stylesheet link from navigation

--- Student/Ferdinard/week1.php
+++ Student/Ferdinard/week1.php
@@ -1,11 +1,12 @@
-<?php
-include ('required.php');
-?>
+<DOCTYPE!>
+<html>
+<?php include ('required.php');?>
 
 <body>
   <h1>Week1-Introduction to HTML</h1>
   <h2>What is HTML?</h2>
 
-<?php
-include ('footer.html');
-?>
+  <?php include ('footer.html');?>
+  <?php include ('script.html');?>
+</body>
+</html>
